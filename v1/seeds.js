9639d427eeb8490389fdef81dd61aae8825d4660
Walter Clayton <walthedude@gmail.com>
2019-12-06 16:00:34 +0100

    changed the source code from seeds.js to async await code, also changed the object data to seeds as its better to have object names in plural

--- v1/seeds.js
+++ v1/seeds.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 var Comment   = require("./models/comment");
 
-var data = [
+var seeds = [
     {
         name: "Cloud's Rest", 
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
@@ -20,68 +20,70 @@ var data = [
     }
 ]
 
-function seedDB(){
-   //Remove all campgrounds
-   Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        }
-        console.log("removed campgrounds!");
-         //add a few campgrounds
-        data.forEach(function(seed){
-            Campground.create(seed, function(err, campground){
-                if(err){
-                    console.log(err)
-                } else {
-                    console.log("added a campground");
-                    //create a comment
-                    Comment.create(
-                        {
-                            text: "This place is great, but I wish there was internet",
-                            author: "Homer"
-                        }, function(err, comment){
-                            if(err){
-                                console.log(err);
-                            } else {
-                                campground.comments.push(comment);
-                                campground.save();
-                                console.log("Created new comment");
-                            }
-                        });
+// function seedDB(){
+//    //Remove all campgrounds
+//    Campground.remove({}, function(err){
+//         if(err){
+//             console.log(err);
+//         }
+//         console.log("removed campgrounds!");
+//          //add a few campgrounds
+//         data.forEach(function(seed){
+//             Campground.create(seed, function(err, campground){
+//                 if(err){
+//                     console.log(err)
+//                 } else {
+//                     console.log("added a campground");
+//                     //create a comment
+//                     Comment.create(
+//                         {
+//                             text: "This place is great, but I wish there was internet",
+//                             author: "Homer"
+//                         }, function(err, comment){
+//                             if(err){
+//                                 console.log(err);
+//                             } else {
+//                                 campground.comments.push(comment);
+//                                 campground.save();
+//                                 console.log("Created new comment");
+//                             }
+//                         });
+//                 }
+//             });
+//         });
+//     }); 
+//     //add a few comments
+// }
+
+
+
+
+
+
+async function seedDB(){
+    try {
+        await Campground.deleteMany({});
+        console.log('Campgrounds removed');
+        await Comment.deleteMany({});
+        console.log('Comments removed');
+
+        for(const seed of seeds) {
+            let campground = await Campground.create(seed);
+            console.log('Campground created');
+            let comment = await Comment.create(
+                {
+                    text: 'This place is great, but I wish there was internet',
+                    author: 'Homer'
                 }
-            });
-        });
-    }); 
-    //add a few comments
+            )
+            console.log('Comment created');
+            campground.comments.push(comment);
+            campground.save();
+            console.log('Comment added to campground');
+        }
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 module.exports = seedDB;
-
-
-
-
-// async function seedDB(){
-//     try {
-//         await Campground.deleteMany({});
-//         console.log('Campgrounds removed');
-//         await Comment.deleteMany({});
-//         console.log('Comments removed');
-
-//         for(const seed of seeds) {
-//             let campground = await Campground.create(seed);
-//             console.log('Campground created');
-//             let comment = await Comment.create(
-//                 {
-//                     text: 'This place is great, but I wish there was internet',
-//                     author: 'Homer'
-//                 }
-//             )
-//             console.log('Comment created');
-//             campground.comments.push(comment);
-//             campground.save();
-//             console.log('Comment added to campground');
-//         }
-//     } catch(err) {
-//         console.log(err);
-//     }
-// }
