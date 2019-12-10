04af4d3aed293a88a337e06cc0328783e6e178a4
Walter Clayton <walthedude@gmail.com>
2019-12-10 21:04:51 +0100

    added the method-override package, added the edit page and linked user id and campground id

--- v1/app.js
+++ v1/app.js
@@ -4,6 +4,7 @@ var express         = require("express"),
     mongoose        = require("mongoose"),
     passport        = require("passport"),
     LocalStrategy   = require("passport-local"),
+    methodOverride  = require("method-override"),
     Campground      = require("./models/campground"),  
     Comment         = require("./models/comment"),
     User            = require("./models/user"),
@@ -24,7 +25,7 @@ mongoose.connect('mongodb://localhost/yelp_camp', {useNewUrlParser: true, useUni
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
+app.use(methodOverride("_method"));
 // seed the database
 // seedDB();
 
